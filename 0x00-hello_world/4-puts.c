a3c4fcccd3fd47466b2928acbb1b0639a3577217
Skynet006 <luckyezealor@gmail>
2022-06-30 15:12:29 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
-#include <studio.h>
+#include <stdio.h>
 #include <string.h>
 
 /**
- * main - Entry point
- * Description: prints "\'Programming is like building a multilingual puzzle"*
- * Return: Always 0 (Success)
- */
+ *  * main:wq - Entry point
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"*
+ *    * Return: Always 0 (Success)
+ *     */
 
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
