0d2ac5e0abc98d63757b2ebf17395e075ba90b91
Skynet006 <luckyezealor@gmail>
2022-06-30 15:41:45 -0700

    finally 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
-#include <string.h>
-
+include < stdio.h >
 /**
- *  * main:wq - Entry point
- *   * Description: prints "\"Programming is like building a multilingual puzzle"*
- *    * Return: Always 0 (Success)
- *     */
-
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
