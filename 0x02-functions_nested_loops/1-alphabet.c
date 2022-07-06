7a62b9e8998435b7f355d5e48c86c09785641330
Skynet006 <luckyezealor@gmail.com>
2022-07-06 15:03:00 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_alphabet - function to print abc
+*
+*Return: 0 success
+*/
+void print_alphabet(void)
+{
+char j;
+
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
