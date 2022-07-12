65fa8674817a40d93de36eff110568bb26b82866
Skynet006 <luckyezealor@gmail.com>
2022-07-12 18:31:28 +0000

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
-*main-check the code
-*@n: integer to be updated
-*Return: Always 0
+*main - check the code
+*@n: takes the functions argument
+*Return: 0
 */
-
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 *n = 98;
 }
