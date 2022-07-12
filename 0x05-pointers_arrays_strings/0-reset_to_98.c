a623a0d2d9ba700012663f9a147ff32ef33ea000
Skynet006 <luckyezealor@gmail.com>
2022-07-12 18:38:40 +0000

    0. 98 Battery st. - correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-*main - check the code
+*reset_to_98 - takes a parameter and updates the value to 98
 *@n: takes the functions argument
 *Return: 0
 */
+
 void reset_to_98(int *n)
 {
 *n = 98;
