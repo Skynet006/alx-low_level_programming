4d2c4e0ef2fe73f316b15ed8c91b1459169206bf
Skynet006 <luckyezealor@gmail.com>
2023-08-03 11:49:05 +0000

    Initial commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * get_bit - get the value of a bit at the given index
- * @n: the number
+ * @n: the number.
  * @index: index starting from 0 of the bit required
  *
  * Return: The converted value
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,7 +3,7 @@
 /**
  * clear_bit - sets a bit at a position to 0
  * @n: number to be used
- * @index: position to be cleared
+ * @index: position to be cleared.
  *
  * Return: 1 on success, -1 on fail
  */
